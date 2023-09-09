75c89f51608d74ed12a2cb5595c16dbe0d6e2874
Dev Dave <143530848+webDev5464@users.noreply.github.com>
2023-09-09 13:52:05 +0530

    type of variable, block scope, statement, keywords -next Operators

---
+++ Apurva/JavaScript/script.js
@@ -0,0 +1,109 @@
+//! ++++++++ Type of Variable's +++++++++
+
+// document.getElementById("secondDemo").innerHTML = "Hello World!"
+
+// let x = "John";
+// const y = "Doe";
+// var z = 26;
+
+// document.getElementById("demo").innerHTML = x + " " + y;
+
+// let fullName = x + " " + y;
+
+// document.getElementById("demo").innerHTML = fullName + "<br>" + "My Age is " + z;
+
+//! ++++++++ Block Scope +++++++++
+
+// let x = "Hello";
+// let x = "Doe";
+// console.log(x);
+
+//? ====================
+
+/**
+ ** let or const follow block scope rule.
+ ** Jobhi variable hum block scope me add karenge vo sirf block scope tak simit rahegi.
+ ** Block scope ke bahar define ki hue value hum bahar hi define karenge.
+ */
+
+// let x = 5;
+
+// {
+//   let x = 7;
+//   console.log(x);
+// 7
+// }
+
+// {
+//   let x = "Hello World!";
+//   console.log(x);
+// Hello World!
+// }
+
+// console.log(x);
+// 5
+
+//? ======================
+
+// const y = 5;
+
+// {
+//   const y = 7;
+//   console.log(y);
+// 7
+// }
+
+// console.log(y);
+// 5
+
+//? ======================
+
+/** Kyu var ka use nahi karenge ?
+ ** same variable name not throw error.
+ ** can not follow block scope.
+ */
+
+// var x = 2;
+// console.log(x);
+
+// {
+//   var x = 5;
+// }
+
+// console.log(x);
+
+//! ++++++++++++ Hot wo define same name variable  ++++++++++++
+
+// let x = 5;
+// let _x = 10;
+// let __x = 15;
+// let ___x = 43;
+// let $x = "Hello";
+// let $$x = "Doe!";
+// let x1 = "Right";
+
+// let #x = "Error"  //* Not valid
+// let 3x = "Error"  //* Not valid
+// let &1 = "Error"  //* Not valid
+// let function = "hello"   //* Not valid
+// let var = 404    //* Not valid
+
+//! +++++++++++ How to use variable's +++++++++++++++++
+
+//* First Method
+// let demo = "memo";
+
+//* Second Method
+// let x, y, z, a, b, c;
+
+// x = 5;
+// y = 10;
+// z = x + y;
+// console.log(x + y);      //* 15
+// console.log(z);          //* 15
+
+// a = "Hello";
+// b = "World!";
+// c = a + " " + b;
+// console.log(c);
+// document.getElementById("demo").innerHTML = c;

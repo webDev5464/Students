d38ad2d756611d22cc36ba4335e4a1ef989ad56b
Dev Dave <143530848+webDev5464@users.noreply.github.com>
2023-09-11 10:42:38 +0530

    types of variable, Block Scope

---
+++ Prerak/JavaScript/FirstFile/script.js
@@ -0,0 +1,90 @@
+//! Debuting
+
+// console.log("Hello World!");
+
+//! Types Of Variable
+
+// let
+// var
+// const
+
+//! Hot To use variable ?
+
+// let Name = "Hello World!";
+// const userName = "John Doe!";
+// var password = "1234Pass";
+
+// console.log(Name);
+// console.log(userName);
+// console.log(password);
+
+//! Block Scope
+
+//? let
+
+// {
+//   let name = "JohnDoe";
+//   console.log(name);
+// }
+
+// console.log(name);
+// // Trow Err
+
+// {
+//   let name = "Chagan Chapri";
+//   console.log(name);
+// }
+
+// var x = 5;
+// console.log(x);
+
+// var x = 8;
+// console.log(x);
+
+// let x = 5
+// let x = 5
+
+//! Do not use var
+
+//? var
+// {
+//   var x = 7;
+// }
+
+// console.log(x);
+
+// {
+//   var x = 11;
+// }
+
+// console.log(x);
+
+//? const
+
+// {
+//   const x = 11;
+//   console.log(x);
+// }
+
+// {
+//   const x = 22;
+//   console.log(x);
+// }
+
+//! difference const & let
+
+//? let
+
+// let x = 12;
+// console.log(x);
+
+// x = 22;
+// console.log(x);
+
+//? const
+
+// const y = 22;
+// console.log(y);
+
+// y = 33;
+// console.log(y);

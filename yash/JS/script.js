ab410c938b6424e8ddf9f6710ed5389181d5c081
Dev Dave <143530848+webDev5464@users.noreply.github.com>
2023-09-12 18:00:08 +0530

    Basic

---
+++ yash/JS/script.js
@@ -0,0 +1,36 @@
+// const x = "Hello World!"
+// document.getElementById("demo").innerHTML = x
+
+// let x = "Hello World!";
+// console.log(x)
+
+// {
+//   let x = 5;
+// console.log(x);
+// }
+
+// {
+//   let x = 12;
+// }
+
+// console.log(x);
+
+// {
+//   const y = "Hello";
+//   console.log(y);
+// }
+
+// {
+//   const y = "World";
+// }
+
+// {
+//   var x = 9;
+// }
+
+// {
+//   console.log(x);
+//   var x = 6;
+// }
+
+// console.log(x);

4a7ad4e490f8b4f292aadfc6ab7decf0467cbfb3
Dev Dave <143530848+webDev5464@users.noreply.github.com>
2023-09-13 12:16:52 +0530

    .

--- yash/JS/script.js
+++ yash/JS/script.js
@@ -34,3 +34,37 @@
 // }
 
 // console.log(x);
+
+// let x = 5
+// let $x = 10
+// let _x = 12
+// let $$$x = 122
+// let ____x = 14
+// let x1 = ""
+
+// let 1x = 12 // Wrong
+// let x-1 = 33 // Wrong
+
+// let x = 5;
+// let y = 2;
+// let z = x + y;
+
+// console.log(z);
+
+/* This 
+is 
+    a 
+    Multiple line
+    Comment
+*/
+
+// const x = 10
+// const y = x + 20
+// console.log(y);
+
+// x = 20
+// console.log(x);
+
+let x = 10
+x += 6
+console.log(x);

a4aa520352c3b6d14e87928e3738c722ffef1b2f
Dev Dave <143530848+webDev5464@users.noreply.github.com>
2023-09-18 17:45:31 +0530

    Introduction if Js

---
+++ Hemang/Js/script.js
@@ -0,0 +1,9 @@
+// document.getElementById("demo").innerHTML = "Hello JavaScript";
+
+// let myPhoneBox = "Samsung Galaxy A50s";
+// document.getElementById("demo").innerHTML = myPhoneBox;
+// document.getElementById("demo").innerHTML = "<h1>" + myPhoneBox + "</h1>";
+
+//! Debugging
+
+// console.log("Hello Hemang")

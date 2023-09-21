fbe2dc3bfdab905b8d0c5433822595ded064035b
Dev Dave <143530848+webDev5464@users.noreply.github.com>
2023-09-21 12:04:35 +0530

    Forward End Backward , set music title end image

--- Jigar/JavaScript/Project/musicSite/audio.js
+++ Jigar/JavaScript/Project/musicSite/audio.js
@@ -6,37 +6,41 @@ const playlist = [
     singer: "arijit singh",
     audioImg: "./audioimg/devaimg.jpeg",
   },
-  //   {
-  //     id: "2",
-  //     name: "Nayan ne bandh rakhi",
-  //     path: "./audio/Nayan-Ne-Bandh-Rakhine(PaglaSongs).mp3",
-  //     singer: "darshan raval",
-  //     audioImg: "./audioimg/nayan-ne-bandh-rakhine.jpg",
-  //   },
-  //   {
-  //     id: "3",
-  //     name: "Raabta",
-  //     path: "./audio/Raabta Agent Vinod 128 Kbps.mp3",
-  //     singer: "arijit singh",
-  //     audioImg: "./audioimg/Rabta-Agent-Vinod.jpg",
-  //   },
-  //   {
-  //     id: "4",
-  //     name: "audio/Nighaho Me Dekho Meri Jo Hai Bas Gaya",
-  //     path: "./audio/audio/Nighaho-Me-Dekho-Meri-Jo-Hai-Bas-Gaya(PagalWorld).mp3",
-  //     singer: "atif aslam",
-  //     audioImg: "./audioimg/nigah.jpeg",
-  //   },
-  //   {
-  //     id: "5",
-  //     name: "Teri Deewani",
-  //     path: "./audio/Teri-Deewani(PaglaSongs).mp3",
-  //     singer: "Kailash Kher",
-  //     audioImg: "./audioimg/teri deewani.jpeg",
-  //   },
+  {
+    id: "2",
+    name: "Nayan ne bandh rakhi",
+    path: "./audio/Nayan-Ne-Bandh-Rakhine(PaglaSongs).mp3",
+    singer: "darshan raval",
+    audioImg: "./audioimg/nayan-ne-bandh-rakhine.jpg",
+  },
+  {
+    id: "3",
+    name: "Raabta",
+    path: "./audio/Raabta Agent Vinod 128 Kbps.mp3",
+    singer: "arijit singh",
+    audioImg: "./audioimg/Rabta-Agent-Vinod.jpg",
+  },
+  {
+    id: "4",
+    name: "Nighaho Me Dekho Meri Jo Hai Bas Gaya",
+    path: "./audio/Nighaho-Me-Dekho-Meri-Jo-Hai-Bas-Gaya(PagalWorld).mp3",
+    singer: "atif aslam",
+    audioImg: "./audioimg/nigah.jpeg",
+  },
+  {
+    id: "5",
+    name: "Teri Deewani",
+    path: "./audio/Teri-Deewani(PaglaSongs).mp3",
+    singer: "Kailash Kher",
+    audioImg: "./audioimg/teri deewani.jpeg",
+  },
 ];
 
 let playPause = document.getElementById("playPause");
+let musicTitle = document.getElementById("musicTitle");
+let musicImg = document.getElementById("musicImg");
+
+musicImg.style.backgroundColor = "100px";
 
 let loadMusic = document.createElement("audio");
 let musicIndex = 0;
@@ -44,6 +48,8 @@ let musicCondition = false;
 
 const musicHandler = (i) => {
   loadMusic.src = playlist[i].path;
+  musicTitle.innerHTML = playlist[i].name;
+  musicImg.src = playlist[i].audioImg;
   // console.log((loadMusic.src = playlist[i].path));
 
   loadMusic.load();
@@ -69,4 +75,29 @@ const PauseMusic = () => {
   musicCondition = false;
   loadMusic.pause();
   playPause.innerHTML = '<i class="fa-solid fa-play"></i>';
+};
+
+const forwardBtn = () => {
+  if (musicIndex < playlist.length - 1) {
+    musicIndex += 1;
+  } else {
+    musicIndex = 0;
+  }
+
+  musicHandler(musicIndex);
+  // console.log("====================================");
+  // console.log(musicIndex);
+  // console.log("====================================");
+  PlayMusic();
+};
+
+const backwardBtn = () => {
+  if (musicIndex > 0) {
+    musicIndex -= 1;
+  } else {
+    musicIndex = playlist.length - 1;
+  }
+
+  musicHandler(musicIndex)
+  PlayMusic()
 };

0120314b8f0f170c076ad1c11bbc1d9bc89d673c
Dev_eLoper <143530848+webDev5464@users.noreply.github.com>
2024-01-17 18:03:41 +0530

    .

---
+++ ayush_aaryan/onlyBackend/backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send("Hello Express")
+})
+
+const PORT = 1010
+app.listen(PORT, () => {
+  console.log(`Server Started from : http://localhost:${PORT}`);
+})

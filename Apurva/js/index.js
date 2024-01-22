5ed366eed621a766d30bb880a9a0b7af72a36565
Dev_eLoper <143530848+webDev5464@users.noreply.github.com>
2024-01-22 15:38:14 +0530

    .

---
+++ Apurva/js/index.js
@@ -0,0 +1,82 @@
+/*
+  * Install nodemon for terminal run automatically :- npm install nodemon
+  * Reinstall node_modules :- npm install
+*/
+
+// document.getElementById("demo").innerHTML = "Hello World!"
+
+//! type's of variables
+
+/*
+  const, let, (var : ES5)
+*/
+
+// const x = 5
+// let y = 6
+// var a = 10
+
+// document.getElementById("demo").innerHTML = x
+// console.log(x)
+// console.log(y);
+
+//? how to use variable
+
+// const x = 10 //! ERR
+// const x = 20 //! ERR
+// var x = 20 //! ERR
+
+// console.log(x);
+
+// var a = 5
+
+// var a = 12
+
+// var a = "Chagan"
+
+// console.log(a);
+
+//? what is different let or const
+
+// let y = 10
+
+// y = 20 + y
+// y += 20
+
+// console.log(y);
+
+// const x = 5
+
+// x = 20
+
+// console.log(x);
+
+//! Block Scope
+
+// const x = 5
+// const y = 10
+
+// {
+//   console.log(y); // 10
+//   const x = 20
+//   console.log(x); // 20
+// }
+
+// console.log(x); // 5
+
+//? var using in blockScope
+
+// var x = 'chagan'
+
+// {
+
+//   console.log(x);
+
+//   var x = 'magan'
+
+// }
+
+// console.log(x); // magan
+
+/*
+? ========== Welcome let, const =============
+*/

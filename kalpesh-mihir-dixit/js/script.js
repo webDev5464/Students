fb96e67529dfcd4da8522037ecc4b67928e3001c
Dev_eLoper <143530848+webDev5464@users.noreply.github.com>
2024-02-06 17:27:55 +0530

    06022024|0527

---
+++ kalpesh-mihir-dixit/js/script.js
@@ -0,0 +1,179 @@
+// document.getElementById('heading').innerHTML = "Hello World!"
+
+// console.log("Hello World!")
+
+//! types of variable
+
+// var variableName = 5
+
+// console.log(variableName);
+
+// let chagan = 5
+// let _12chagan12 = "Hello"
+
+// console.log(chagan);
+
+//? variable type
+
+/*
+  var
+  const
+  let
+*/
+
+// var x = 5
+
+
+// var x = 10
+
+// console.log(x);
+
+//! blockScope
+
+// let x = 5
+
+{
+  // let x = 10
+
+  // console.log(x);
+  // console.log(x);
+}
+
+// console.log(x);
+
+
+// function myFunction() {
+// let y = 5
+// console.log(y);
+// }
+
+// myFunction()
+
+//? var
+// var m = 5
+
+// {
+//   console.log(m);
+//   var m = 9
+// }
+
+// console.log(m);
+
+/*
+? let
+? const
+*/
+
+// let x = 5
+
+// console.log(x); // 5
+
+// x = 10
+
+// console.log(x); // 10
+
+// const x = 5
+
+// console.log(x);
+
+// x = 10
+
+// console.log(x);
+
+//! Arithmetic operator
+
+// let x = 5
+
+// console.log(x + 6);
+// console.log(x - 6);
+// console.log(x * 6);
+// console.log(x / 6);
+// console.log(x % 2);
+// console.log(x ** 2);
+
+// x++
+// x--
+// console.log(x);
+
+//! assignment operator
+
+// let x = 5
+
+// console.log(x += 5);
+// console.log(x -= 10);
+// console.log(x *= 10);
+// console.log(x /= 10);
+// console.log(x %= 10);
+// console.log(x **= 10);
+
+//? QnA
+
+// let x = 5
+// let y = "10"
+
+// console.log(typeof (y + x)); // 105
+
+// console.log(x + 15 + y); // 2010
+
+//! Datatypes
+
+/*
+  Number
+  String
+  Boolean
+  Object - array, object, date, null
+*/
+
+// let age = 22 // Number
+// console.log(typeof age);
+
+// let person = "Chagan" // String
+// console.log(typeof person);
+
+// let x = 5
+
+// console.log(typeof Number(x));
+// console.log(typeof String(x));
+
+// console.log(Number(false)); // 0
+// console.log(Number(true)); // 1
+
+//? Object - array
+
+// const person = ["Chagan", 5, "John", "Duck"]
+
+// console.log(typeof person);
+
+// console.log(Array.isArray(person));
+
+// console.log(person[1]);
+
+//? object
+
+// const user = { firstName: "John", lastName: "Doe" }
+
+// console.log(user.firstName + " " + user.lastName);
+
+//? date
+
+// let today = new Date()
+// console.log(today);
+
+//? null
+
+// console.log(null);
+
+//? NaN (not a number)
+
+// console.log(typeof NaN);
+
+//? undefine
+
+// let chagan;
+// console.log(typeof chagan);
+
+// let x = null
+
+// x = [5, 6]
+
+// console.log(x);

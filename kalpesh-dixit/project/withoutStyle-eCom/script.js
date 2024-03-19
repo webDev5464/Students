e75a50ddbff6c36187b4fed56da39610564cea67
Dev_eLoper <143530848+webDev5464@users.noreply.github.com>
2024-03-19 16:20:25 +0530

    19032024|1620

--- kalpesh-dixit/project/withoutStyle-eCom/script.js
+++ kalpesh-dixit/project/withoutStyle-eCom/script.js
@@ -1,4 +1,19 @@
 const fetchingData = async () => {
+  const renderData = document.getElementById("renderData")
+  const rowData = await fetch("https://res.cloudinary.com/dpiiduvvx/raw/upload/v1703580765/API/productsAPI")
+  const objData = await rowData.json()  
+  
+  renderData.innerHTML = objData.earbud.map((x) => {
+    return `
+      <div>
+        <img src="${x.img1}" alt="" width="400px">
+      </div>
+      <p>${x.title}</p>
+    `
+  }).join("")
 
+  console.log(objData.earbud);
 }
 fetchingData()
+
+console.log(document.getElementById("search").value);

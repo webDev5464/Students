8496efaa7f8f1250d5a6645d3b39a01c19412a83
Dev_eLoper <143530848+webDev5464@users.noreply.github.com>
2024-05-01 16:45:33 +0530

    01052024|0445

---
+++ kalpash/react/basic/src/App.jsx
@@ -0,0 +1,36 @@
+// This is a function base component
+
+// function App() {
+//   return <h1>Hello World!</h1>
+// }
+
+// export default App
+
+// =======================
+
+// export default function App() {
+//   return <h1>Hello World!</h1>
+// }
+
+
+// =======================
+
+
+// export const App = () => {
+//   return <h1>Hello World!</h1>
+// }
+
+const App = () => {
+  return <h1 className="text-6xl font-bold bg-red-500 my-5">Hello World!</h1>
+}
+
+const Chagan = () => {
+  return <h1>My Name is chagan</h1>
+}
+
+export { App, Chagan }
+
+
+// =======================
+
+// export default App
---
+++ kalpash/react/basic/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App, Chagan } from './App.jsx'
+import "./Style.css"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <Chagan />
+  </React.StrictMode>,
+)
